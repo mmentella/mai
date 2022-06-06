5ac690cb991513a006831c621abe6998f9f1dab0
Matteo Mentella <mtmentella@invitalia.it>
2022-06-06 05:02:36 +0200

    SIMD

--- maiTests/network/MNISTTest.cs
+++ maiTests/network/MNISTTest.cs
@@ -1,11 +1,43 @@
-﻿using mai.mnist;
+﻿using FluentAssertions;
+using mai.blas;
+using mai.mnist;
 using mai.network;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.Intrinsics;
 using Xunit;
 
 namespace maiTests.network
 {
     public class MNISTTest
     {
+        [Fact]
+        public void SIMDSumTest()
+        {
+            Matrix matrix = (Matrix)(Enumerable.Range(1, 10000).Select(r => (double)r).ToArray());
+
+            double sum = matrix.Sum();
+            double simdsum = matrix.SIMDSum();
+
+            simdsum.Should().Be(sum);
+        }
+
+        [Fact]
+        public void ImplicitCastExplicitCastReshapeTest()
+        {
+            double[] data = { 1d, 2d, 3d, 4d, 5d, 6d };
+            Matrix matrix = (Matrix)data;
+            matrix.Reshape(2, 3);
+
+            matrix.Rows.Should().Be(2);
+            matrix.Columns.Should().Be(3);
+
+            data = (double[])matrix;
+
+            data.Length.Should().Be(6);
+        }
+
         [Fact]
         public void MNISTSoftmaxCrossEntropyLossTest()
         {
