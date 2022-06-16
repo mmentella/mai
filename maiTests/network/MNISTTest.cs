2a8e3434343f9629920eaf3b72d9184ec80845b9
Matteo Mentella <mtmentella@invitalia.it>
2022-06-16 08:41:25 +0200

    dropout

--- maiTests/network/MNISTTest.cs
+++ maiTests/network/MNISTTest.cs
@@ -12,7 +12,8 @@ namespace maiTests.network
             int trainingSamples = 100;
             int validationSamples = trainingSamples / 6;
 
-            NeuralNetwork network = new(new Layer[] {new Dense(89, activation:new Sigmoid()),
+            NeuralNetwork network = new(new Layer[] {new Dense(89, activation:new Sigmoid(), dropout:1),
+                                                     //new Dense(46, activation:new Sigmoid(), dropout:0.9),
                                                      new Dense(10, activation:new Linear())
                                                     }, new SoftmaxCrossEntropyLoss(), seed: 20220603);
             Optimizer sgd = new SGDMomentum(network, learningRate: 0.9d);
