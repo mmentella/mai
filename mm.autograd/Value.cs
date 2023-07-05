3309220da7d1284b1a1ddc3567e970d0cc85ff1f
Matteo Mentella <mtmentella@invitalia.it>
2023-07-05 22:00:00 +0200

    pytorch

---
+++ mm.autograd/Value.cs
@@ -0,0 +1,39 @@
+﻿namespace mm.autograd;
+
+public class Value
+    : IComparable<Value>
+{
+    private readonly double data;
+    private readonly string? name;
+    private readonly Value[] children;
+    private readonly double gradient;
+
+    private Action? backward;
+
+    public Value(double value, string? name = null, params Value[] children)
+    {
+        backward = null;
+        this.data = value;
+        this.name = name;
+        this.children = children;
+
+        gradient = 0;
+    }
+
+    public double Data => data;
+
+    public double Gradient => gradient;
+
+    public Value[] Children => children;
+
+    public static implicit operator Value(double value) => new(value);
+    public static implicit operator Value(int value) => new(value);
+    public static implicit operator Value(float value) => new(value);
+
+    public int CompareTo(Value? other) => data.CompareTo(other?.data);
+
+    public override string ToString()
+    {
+        return $"value={data}";
+    }
+}
