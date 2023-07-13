2ca6b30a94fcc09789cf9558af53188c324476b2
Matteo Mentella <mtmentella@invitalia.it>
2023-07-13 07:04:45 +0200

    lda

--- pytorch/lda-tal.py
+++ pytorch/lda-tal.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from plot import plot_decision_regions
 
 df = pd.read_csv("pytorch\\data\\tal.csv", header=None)
 X, y = df.iloc[:, 0:4].values, df.iloc[:, 4].values
@@ -14,17 +17,20 @@ scaler = StandardScaler()
 X_train_std = scaler.fit_transform(X_train)
 X_test_std = scaler.transform(X_test)
 
-labels = np.unique(y_test)
-mean_vecs = []
+lda = LinearDiscriminantAnalysis(n_components=2)
+labels = dict.fromkeys(y_train)
+i = 1
 for l in labels:
-    mean_vecs.append(np.mean(X_train_std[y_train == l], axis=0))
+    labels[l] = i
+    i+=1
+l_train = np.empty_like(y_train)
+for idx,y in enumerate(y_train):
+    l_train[idx] = labels[y]
+X_train_lda = lda.fit_transform(X_train_std, l_train)
 
-d = 4  # number of features
-S_W = np.zeros((d, d))
-for label, mv in zip(labels, mean_vecs):
-    class_scatter = np.cov(X_train_std[y_train == label].T)
-    S_W += class_scatter
-# print(S_W)
+lr = LogisticRegression(multi_class="ovr", random_state=1, solver="lbfgs")
+lr.fit(X_train_lda, l_train)
 
-mean_overall = np.mean(X_train_std, axis=0)
-mean_overall = mean_overall.reshape(d, 1)
+plot_decision_regions(X_train_lda, l_train, classifier=lr)
+plt.tight_layout()
+plt.show()
