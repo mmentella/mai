7111da33295bc149d36ca80fb3b8474ed13dde7b
Matteo Mentella <mtmentella@invitalia.it>
2023-07-18 22:30:48 +0200

    tal.csv label class

--- pytorch/csv-tal.py
+++ pytorch/csv-tal.py
@@ -1,9 +1,51 @@
 import glob
 import pandas as pd
 
-files = glob.glob("pytorch\\data\\*.csv");
-df_list = (pd.read_csv(file,names=["id","open","high","low","close","range","m.open","m.high","m.low","m.close","m.range","tal","sal","kal","tas","sas","kas","lookback"]) for file in files)
-df   = pd.concat(df_list, ignore_index=True)
+files = glob.glob("pytorch\\data\\*.csv")
+df_list = (
+    pd.read_csv(
+        file,
+        names=[
+            "id",
+            "open",
+            "high",
+            "low",
+            "close",
+            "range",
+            "m.open",
+            "m.high",
+            "m.low",
+            "m.close",
+            "m.range",
+            "tal",
+            "sal",
+            "kal",
+            "tas",
+            "sas",
+            "kas",
+            "lookback",
+        ],
+    )
+    for file in files
+)
+df = pd.concat(df_list, ignore_index=True)
 
-df = df.loc[:,["open","high","low","close","range","m.open","m.high","m.low","m.close","m.range","tal"]]
-df.to_csv("pytorch\\data\\tal.csv",index=False,header=True)
+df = df.loc[
+    :,
+    [
+        "open",
+        "high",
+        "low",
+        "close",
+        "range",
+        "m.open",
+        "m.high",
+        "m.low",
+        "m.close",
+        "m.range",
+        "tal",
+    ],
+]
+df['tal']=40*df['tal']
+df['tal']=df['tal'].astype('int')
+df.to_csv("pytorch\\data\\tal.csv", index=False, header=True)
